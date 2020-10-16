f1b65792e68be1ba4866cc78a50f10b67c0b3bf4
Walter dos Santos <walter4asantos@gmail.com>
2020-10-16 14:58:11 -0300

    Insercao com recuperacao de multiplos Ids

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,7 +20,7 @@ public class Program {
 		try {
 			conn = DB.getConnection();
 			
-			st = conn.prepareStatement(
+/*			st = conn.prepareStatement(
 					"INSERT INTO seller "
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
 					+ "VALUES "
@@ -32,6 +32,11 @@ public class Program {
 			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
 			st.setDouble(4, 3000.00);
 			st.setInt(5, 4);
+			*/
+			
+			st = conn.prepareStatement(
+					"insert into department (Name) values ('D1'),('D2')",
+					Statement.RETURN_GENERATED_KEYS);
 			
 			int rowsAffected = st.executeUpdate();
 			
@@ -49,9 +54,9 @@ public class Program {
 		catch (SQLException e) {
 			e.printStackTrace();
 		}
-		catch (ParseException e) {
+/*		catch (ParseException e) {
 			e.printStackTrace();
-		}
+		} */
 		finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
